e29b902010a9e5d2ede423100d47da1a83d27dbd
ricomanifesto <michaelrico124@gmail.com>
2025-05-04 19:59:41 -0500

    adding base url

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,13 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import path from 'path';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/', // For custom domain
+  base: '/', // This tells Vite to use root-relative URLs
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
     },
   },
-});
+})
