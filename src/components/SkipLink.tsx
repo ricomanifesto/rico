c300c8a0065fdd94c9b613a12c13b38d467941e0
ricomanifesto <michaelrico124@gmail.com>
2026-06-26 19:05:43 -0500

    Use shared skip link focus token

--- src/components/SkipLink.tsx
+++ src/components/SkipLink.tsx
@@ -2,7 +2,7 @@ export default function SkipLink() {
   return (
     <a
       href="#main-content"
-      className="sr-only focus:not-sr-only focus:fixed focus:left-4 focus:top-4 focus:z-[60] focus:rounded focus:bg-slate-950 focus:px-4 focus:py-2 focus:text-sm focus:font-semibold focus:text-white focus:outline focus:outline-2 focus:outline-offset-2 focus:outline-[#66b2ff]"
+      className="skip-link sr-only"
     >
       Skip to main content
     </a>
